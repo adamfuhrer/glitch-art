1f1aacff8366e2491830e3e0b699ac06ae7d9bad
adamfuhrer <adam.fuhrer@caseware.com>
2020-02-02 13:43:45 -0500

    move remove color button outside of the square into its own distinct element

--- src/app/components/color-picker/color-picker.component.ts
+++ src/app/components/color-picker/color-picker.component.ts
@@ -35,28 +35,4 @@ export class ColorPickerComponent {
       this.removeClicked.emit();
     }, 350);
   }
-
-  // Returns a fill color based on the hex value of the background
-  getTextColorHex(backgroundColor: string) {
-    const hexRegex = /^#?[a-f0-9]{6}$/i;
-    const rgbRegex = /[a-f0-9]{2}/gi;
-
-    if (hexRegex.test(backgroundColor)) {
-      const rgb = backgroundColor.match(rgbRegex);
-      const responseColor: number[] = [];
-      let luminosity: number;
-
-      for (let i = 0; i < rgb.length; i++) {
-        const part = parseInt(rgb[i], 16) / 255;
-        if (part <= 0.03928) {
-          responseColor.push(part / 12.92);
-        } else {
-          responseColor.push(Math.pow((part + 0.055) / 1.055, 2.4));
-        }
-      }
-
-      luminosity = 0.2126 * responseColor[0] + 0.7152 * responseColor[1] + 0.0722 * responseColor[2];
-      return luminosity > 0.279 ? '#222222' : '#FFFFFF';
-    }
-  }
 }
